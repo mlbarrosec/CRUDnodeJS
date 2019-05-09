8956a4b56f5a5eecf842b16456ee70545316ab6d
Marcelo Lopes de Barros <marcelo.barros@pampa.compasso>
2019-05-09 11:31:42 -0300

    implemnt  list os all users

--- db_configs/dbRequests.ts
+++ db_configs/dbRequests.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import {createConnection, getRepository} from "typeorm";
 import {User} from "./entity/User";
 
 const connection = createConnection ({
@@ -14,6 +14,8 @@ const connection = createConnection ({
     ],
     synchronize: true
 });
+
+
 
 export class dbRequests {
 
@@ -44,6 +46,99 @@ export class dbRequests {
                 }         
                 res.status(400).send(saidaErro);
                 console.log(error);
+        });
+    }
+
+    //Function to list all users in data base
+    //return in response a template for table html mount
+    listUsers(res:any):any {
+        connection
+        .then(async connection => {
+            let allUsers = connection.getRepository(User);
+            let users = await allUsers.find();
+
+            let template =  `
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+                    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+                    <title>CRUD NODE JS</title>
+                </head>           
+                
+                <body>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">                        
+                                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                                    <a class="navbar-brand" href="/">CRUD</a>
+                                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                    </button>
+                                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                                        <div class="navbar-nav">
+                                        <a class="nav-item nav-link active" href="/">HOME <span class="sr-only">(current)</span></a>
+                                        <a class="nav-item nav-link" href="/insert">INSERT</a>
+                                        <a class="nav-item nav-link" href="/users-list">USERS LIST</a>                      
+                                        </div>
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="row">
+                            <div class="col-md-7">
+                                <table class="table">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th class="text-center">Id</th>
+                                            <th class="text-center">Nome</th>
+                                            <th class="text-center">Username</th>
+                                            <th class="text-center">Email</th>
+                                            <th class="text-center">Address</th>
+                                            <th class="text-center">Phone</th>
+                                            <th class="text-center">Editar</th>
+                                            <th class="text-center">Excluir</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>                            
+                                        ${users.map(user => `
+                                            <tr>
+                                                <td class="text-center">${user.id}</td>
+                                                <td class="text-center">${user.name}</td>
+                                                <td class="text-center">${user.username}</td>
+                                                <td class="text-center">${user.email}</td>
+                                                <td class="text-center">${user.address}</td>
+                                                <td class="text-center">${user.phone}</td>
+                                                <td class="text-center"><i class="fas fa-trash-alt"></i></td>
+                                                <td class="text-center"><i class="fas fa-pen-alt"></i></td>
+                                            </tr>`).join('')}
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div class="col-md-5"></div>
+
+                        </div>
+                    </div>        
+                </body>
+                <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+                <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+            </html>    
+            `;
+            
+            res.send(template);
+        })
+        .catch(error => {
+            let saidaErro = {
+                "errorCode":"400",
+                "msg": 'Error connect to database'
+            }         
+            res.status(400).send(saidaErro);
+            console.log(error);
         });
     }
 
--- routes/crudRoutes.js
+++ routes/crudRoutes.js
@@ -1,16 +1,31 @@
 module.exports = function(app) { 
 
+    //Routes for Views
     app.get("/", function(req,res){ 
-        res.sendfile(__dirname + '/views/index.html');
+        res.sendFile(__dirname + '/views/index.html');
     });
 
     app.get("/insert", function(req,res){ 
-        res.sendfile(__dirname + '/views/insert.html');
+        res.sendFile(__dirname + '/views/insert.html');
     });
 
     app.get("/success",function(req,res){
-        res.sendfile(__dirname + '/views/success.html');
+        res.sendFile(__dirname + '/views/success.html');
     })
+
+    app.get("/users-list" ,function(req,res){
+        //res.sendFile(__dirname + '/views/users-list.html'); 
+        let connection = new app.src.dbRequests();
+        connection.listUsers(res);    
+    });
+
+    //Routes dor Queryes
+   /* app.get('/users-list/:id', function(req,res){
+
+        let id = req.params.id;
+        let connection = new app.src.dbRequests();
+        connection.listUsers(id, res);
+    });*/
 
     app.post("/insert",function(req,res){        
 
@@ -19,6 +34,8 @@ module.exports = function(app) {
 
         connection.insertUser(dataUser,res)
     });
+
+    
 
 
 }
